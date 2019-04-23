1389f0a81d83d284593ae13adafd5f840a65f0e1
Álex Junio <alexshox@gmail.com>
2019-04-22 21:22:05 -0300

    basico de IF   e ELSe

---
+++ condicional.js
@@ -0,0 +1,13 @@
+//if (boo){}
+
+var x = 18;
+var y = 18 ;
+
+if (x > y) {
+    console.log("X maior que Y")
+}
+else  if (x== y){
+    console.log("Y igual X")
+}else {
+    console.log("Y maior que X")
+}
