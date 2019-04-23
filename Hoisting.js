e7acd98e476460c1e4c0bd92c90289d75a80a303
Álex Junio <alexshox@gmail.com>
2019-04-23 20:38:42 -0300

    coiseitos basicos de hoistging

---
+++ Hoisting.js
@@ -0,0 +1,8 @@
+// hoisting  é tipo o JS jogar todas as declaraçoes de objetos para o inicio do codigo 
+// em tempo de execução
+
+console.log(x); //neste caso ele aceita pois a variavel não foi definida com um valor
+
+var x = 10;
+
+console.log(x);
